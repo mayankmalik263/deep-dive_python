cffbd231548b17f3d18178be10de1e60b25908a3
Mayank Malik <mayankmalik263@gmail.com>
2024-12-20 21:09:05 +0530

    Version 2 : Enhance login validation by checking for valid email format and improving password prompts

--- 11. if_else + loops.py
+++ 11. if_else + loops.py
@@ -2,15 +2,19 @@
 # correct password = 123456
 
 email = input("Enter your email: ")
-password = input("Enter your password: ")
-if email == "mayankmalik@gmail.com" and password == "123456":
-    print("Welcome to your account")
-elif email == "mayankmalik@gmail.com" and password != "123456":
-    print("Invalid password")
-    password = input("Enter your password again: ")
-    if password == "123456":
-        print("Welcome to your account")
+if '@' in email:
+    password = input("Enter your password: ")
+    if email == "mayankmalik@gmail.com" and password == "123456":
+            print("Welcome to your account")
+    elif email == "mayankmalik@gmail.com" and password != "123456":
+            print("Invalid password")
+            password = input("Enter your password again: ")
+            if password == "123456":
+                print("Welcome to your account")
+            else:
+                print("Invalid password again!")
     else:
-        print("Invalid password again!")
+            print("Invalid email or password")
 else:
-    print("Invalid email or password")
+    print("Invalid email")
+    
