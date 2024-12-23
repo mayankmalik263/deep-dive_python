14eda0d06ab47baf641a3694fcd1ae977d06c2e6
Mayank Malik <mayankmalik263@gmail.com>
2024-12-23 23:52:47 +0530

    Learned about different built-in functions

---
+++ 14. built-in functions.py
@@ -0,0 +1,23 @@
+# all the built-in functions :
+
+print("Hello")
+input("Enter your name: ")
+print(type(True))
+print(int(4.54321321))
+print(abs(-9))
+print(pow(2,3))
+print(min([1,2,3,4,0]))
+print(max("Rohtak"))
+print(round(8.97))
+print(divmod(5,4)) #(1,1) -> (5//4,5%4)
+print(bin(81))
+print(oct(81))
+print(hex(81))
+a=3
+print(id(a)) # to get the address of any variable in which a value is stored in the memory
+print(ord('A')) # return the ASCII code of any character
+print(len('Rohtak')) # gives you the length of the iterables
+print(sum([12,342,3213,41])) # gives you the total some of the values in any iterables except string ofc
+print(help('print')) # gives you the official documentation and details of any function that you put in the help function
+
+
